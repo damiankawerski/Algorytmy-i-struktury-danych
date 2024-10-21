309b325553d4a52190e1a9cf1d40058870a86cbb
Dyniek <damian.kawerski@gmail.com>
2024-10-21 23:07:27 +0200

    Remake

--- Kolejka/main.cpp
+++ Kolejka/main.cpp
@@ -1,29 +1,31 @@
 #include <iostream>
 
-// p to jest czas w sekundach od początku symulacji albo od ostatniego klienta w kolejce
-// r - liczba produktów w koszyku
-// a - id kasy
-// z - time for client to pay
 using namespace std;
 
-struct node {
-    int p;              // time in seconds from the beginning of the simulation or from the last client in the queue
-    int r;              // number of products in the basket
-    node* next;
+// t - czas trwania symulacji
+// i - liczba kolejek
+// s - sprawność, ile czasu na jeden produkt
+// z - czas płatności
+// p - czas od rozpoczęcia symulacji albo od ostatniego klienta
+// r - liczba produktów
+// a - numer kasy
+// f - flaga
+
+struct client_node {
+    int p, r;
+    client_node* next;
 };
 
-class fifo_List {
+class fifo_queue {
+private:
+    client_node *head;
+    client_node *tail;
+    int size;
 public:
-    node *head, *tail;
-    int id;             // id of the queue
-    char f;             // flag of status in the queue in k, o, z
-    int time_to_end;    // time to end of the queue
-
-    fifo_List(int id) {
+    fifo_queue() {
         head = nullptr;
         tail = nullptr;
-        this->id = id;
-        time_to_end = 0;    // this might be bad idea
+        size = 0;
     }
 
     bool is_empty() {
@@ -31,55 +33,37 @@ public:
     }
 
     void add_last(int r, int p) {
-        node *temp = new node();
-        temp->r = r;
-        temp->p = p;
-        temp->next = nullptr;
+        client_node* new_client = new client_node;
+        new_client->r = r;
+        new_client->p = p;
+        new_client->next = nullptr;
 
-        if(is_empty()) {
-            head = tail = temp;
+        if (is_empty()) {
+            head = new_client;
+            tail = new_client;
         } else {
-            tail->next = temp;
-            tail = temp;
+            tail->next = new_client;
+            tail = new_client;
         }
+        size++;
     }
 
     void remove_first() {
-        if(is_empty()) {
+        if (is_empty()) {
             return;
         }
 
-        node *temp = head;
+        client_node* temp = head;
         head = head->next;
-
-        if(head == nullptr) {
-            tail = nullptr;
-        }
-
         delete temp;
+        size--;
     }
 
-    ~fifo_List() {
-        while(!is_empty()) {
-            remove_first();
-        }
+    int get_size() {
+        return size;
+    }
+
+    client_node* get_head() {
+        return head;
     }
 };
-
-
-int main() {
-    unsigned int t;         // time of simulation
-    unsigned int i;         // number of queues
-    unsigned int s;         // efficienct of queues
-    unsigned int z;         // time for client to pay
-    cin >> t >> i >> s >> z;
-
-    fifo_List **queues = new fifo_List*[i];
-
-    while(t > 0) {
-
-    }
-
-
-    delete[] queues;
-}
