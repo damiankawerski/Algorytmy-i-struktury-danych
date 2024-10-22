d1b2f1870915a1cf3e540c764882b4bfa809bf6c
Damian Kawerski <damian.kawerski@gmail.com>
2024-10-22 18:44:53 +0200

    processing doesnt work

--- Kolejka/cmake-build-debug/cmake_install.cmake
+++ Kolejka/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/damian/CLionProjects/Kolejka
+# Install script for directory: /home/damian/CLionProjects/Algorytmy-i-struktury-danych/Kolejka
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/damian/CLionProjects/Kolejka/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/damian/CLionProjects/Algorytmy-i-struktury-danych/Kolejka/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- Kolejka/main.cpp
+++ Kolejka/main.cpp
@@ -1,3 +1,4 @@
+#include <climits>
 #include <iostream>
 
 using namespace std;
@@ -12,7 +13,8 @@ using namespace std;
 // f - flaga
 
 struct client_node {
-    int p, r;
+    int  r;
+    int time_to_process;
     client_node* next;
 };
 
@@ -32,10 +34,10 @@ public:
         return head == nullptr;
     }
 
-    void add_last(int r, int p) {
+    void add_last(int r, int time_to_process) {
         client_node* new_client = new client_node;
         new_client->r = r;
-        new_client->p = p;
+        new_client->time_to_process = time_to_process;
         new_client->next = nullptr;
 
         if (is_empty()) {
@@ -59,11 +61,154 @@ public:
         size--;
     }
 
-    int get_size() {
-        return size;
-    }
-
     client_node* get_head() {
         return head;
     }
+
+    int get_size() const {
+        return size;
+    }
 };
+
+
+class cash_register {
+public:
+    fifo_queue queue;
+    int efficiency, time_to_pay, time_of_simulation;
+    char f;
+
+    cash_register(int s, int z) {
+        efficiency = s;
+        time_to_pay = z;
+        this->f = 'z';
+    }
+
+    ~cash_register() {
+        while (!queue.is_empty()) {
+            queue.remove_first();
+        }
+    }
+
+    void open_register() {
+        f = 'o';
+    }
+
+    void close_register() {
+        f = 'z';
+        while(!queue.is_empty()) {
+            queue.remove_first();
+        }
+    }
+
+    void add_client(int r) {
+        int waiting_time = r * efficiency + time_to_pay;
+        queue.add_last(r, waiting_time);
+    }
+
+    void process_queue(int p) {
+        client_node* client = queue.get_head();
+        if (client == nullptr) {
+            return;
+        }
+
+        while (p > 0 && client != nullptr) {
+            if (client->time_to_process <= p) {
+                p -= client->time_to_process;
+                queue.remove_first();
+                client = queue.get_head();
+            } else {
+                client->time_to_process -= p;
+                p = 0;
+            }
+        }
+    }
+
+    int get_number_of_people() {
+        return queue.get_size();
+    }
+
+    int get_waiting_time() {
+        client_node* temp = queue.get_head();
+        int waiting_time = 0;
+        while(temp != nullptr) {
+            waiting_time += temp->time_to_process;
+            temp = temp->next;
+        }
+    }
+};
+
+int find_queue_with_lowest_time(cash_register** registers, int i) {
+    int min_time = INT_MAX;
+    int min_index = -1;
+    for (int j = 0; j < i; j++) {
+        if(registers[j]->f == 'o') {
+            if (registers[j]->get_waiting_time() < min_time) {
+                min_time = registers[j]->get_waiting_time();
+                min_index = j;
+            }
+        }
+    }
+    return min_index;
+}
+
+void initialize_registers(cash_register **registers, int i, int s, int z) {
+    for (int j = 0; j < i; j++) {
+        registers[j] = new cash_register(s, z);
+    }
+}
+
+void delete_registers(cash_register **registers, int i) {
+    for (int j = 0; j < i; j++) {
+        delete registers[j];
+    }
+    delete[] registers;
+}
+
+void show_results(cash_register **registers, int i) {
+    for (int j = 0; j < i; j++) {
+        if(registers[j]->f == 'z') {
+            cout << "K" << j << ": z, ";
+        } else if(registers[j]->f == 'o') {
+            cout << "K" << j << ": " << registers[j]->get_number_of_people() << "o " << registers[j]->get_waiting_time() << "s, ";
+        }
+    }
+}
+
+int main() {
+    int t; // czas trwania symulacji
+    int i; // liczba kolejek
+    int s; // sprawność, ile czasu na jeden produkt
+    int z; // czas płatności
+
+    cin >> t >> i >> s >> z;
+
+    cash_register** registers = new cash_register*[i];
+    initialize_registers(registers, i, s, z);
+
+    while(t > 0) {
+        char f; // flaga kolejki
+        int a;   // indeks kolejki
+        int p; // czas od rozpoczęcia symulacji albo od ostatniego klienta
+        cin >> f;
+        if(f == 'o') {
+            cin >> a;
+            registers[a]->open_register();
+        }
+        if(f == 'z') {
+            cin >> a;
+            registers[a]->close_register();
+        }
+        if(f == 'k') {
+            int r;
+            cin >> p >> r;
+            int index = find_queue_with_lowest_time(registers, i);
+            registers[index]->add_client(r);
+            for(int j = 0; j < i; j++) {
+                registers[j]->process_queue(p);
+            }
+            t-=p;
+        }
+    }
+    show_results(registers, i);
+    delete_registers(registers, i);
+}
