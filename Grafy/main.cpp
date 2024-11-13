d193b6eb5f90e3026e1045ca83305b4ccaaa674f
Damian <damian.kawerski@gmail.com>
2024-11-13 16:14:21 +0100

    Coś chodzi

--- Grafy/main.cpp
+++ Grafy/main.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
-#include <stack>
-#include <queue>
-#include <limits>
 #include <algorithm>
-
+#include <queue>
 
 using namespace std;
 
@@ -13,6 +10,7 @@ class graph {
 private:
     unordered_map<int, vector<int>> adj_list;
     int num_vertices;
+
 public:
     graph(int num_vertices) {
         this->num_vertices = num_vertices;
@@ -31,41 +29,127 @@ public:
     }
 
     int get_end() {
+        int counter = 0;
+        int result = -1;
         for(auto i : adj_list) {
-            for(auto j : i.second) {
+            for(int j : i.second) {
                 if(adj_list[j].empty()) {
-                    return j;
+                    counter++;
+                    result = j;
+                    if(counter > 1) {
+                        return -1;
+                    }
                 }
             }
         }
-        return -1;
+        return result;
     }
 
-    vector<int> find_shortest_odd_path(int start) {
-       int end = get_end();
-       stack<int> s;
-       vector<int> result;
+    void dfs(int u, int end, vector<bool>& visited, vector<int>& path, vector<vector<int>>& all_paths) {
+        visited[u] = true;
+        path.push_back(u);
 
-       while(!s.empty()) {
-             s.push(start);
+        if (u == end) {
+            if(path.size() % 2 != 0) {
+                all_paths.push_back(path);
+            }
+        } else {
+            for (int v : adj_list[u]) {
+                if (!visited[v]) {
+                    dfs(v, end, visited, path, all_paths);
+                }
+            }
+        }
 
-       }
+        path.pop_back();
+        visited[u] = false;
     }
+
+    vector<int> bfs(int start, int end) {
+        queue<pair<int, vector<int>>> q;
+        q.push({start, {start}});
+        unordered_map<int, bool> visited;
+
+        while (!q.empty()) {
+            int u = q.front().first;
+            vector<int> path = q.front().second;
+            q.pop();
+
+            if (u == end && path.size() % 2 != 0) {
+                return path;
+            } else {
+                for (int v : adj_list[u]) {
+                    if (find(path.begin(), path.end(), v) == path.end()) {
+                        vector<int> new_path = path;
+                        new_path.push_back(v);
+                        q.push({v, new_path});
+                    }
+                }
+            }
+        }
+        return vector<int>();
+    }
+
+    vector<vector<int>> find_all_paths_dfs(int start, int end) {
+        vector<vector<int>> all_paths;
+        vector<int> path;
+        vector<bool> visited(num_vertices, false);
+
+        dfs(start, end, visited, path, all_paths);
+
+        sort(all_paths.begin(), all_paths.end());
+
+        return all_paths;
+    }
+
+    vector<int> find_path_bfs(int start, int end) {
+        vector<int> path = bfs(start, end);
+        return path;
+    }
+
+    vector<int> find_shortest_odd_path_dfs(int start, int end) {
+       vector<vector<int>> all_paths = find_all_paths_dfs(start, end);
+        if(all_paths.empty()) {
+            return vector<int>();
+        } else {
+            return all_paths[0];
+        }
+    }
+
 };
 
 int main() {
-    graph g(6);
-    g.add_edge(0, 1);
-    g.add_edge(1, 5);
-    g.add_edge(2, 0);
-    g.add_edge(2, 1);
-    g.add_edge(3, 2);
-    g.add_edge(3, 4);
-    g.add_edge(4, 0);
+    std::ios_base::sync_with_stdio(false);
+    std::cout.tie(nullptr);
+    std::cin.tie(nullptr);
 
-    vector<int> result = g.find_shortest_odd_path(3);
-    for(auto i : result) {
-        cout << i << " ";
+    int tests, vertices, edges, start;
+    cin >> tests;
+    while(tests--) {
+        cin >> vertices >> edges >> start;
+        graph g(vertices);
+        for(int i = 0; i < edges; i++) {
+            int u, v;
+            cin >> u >> v;
+            g.add_edge(u, v);
+        }
+        int end = g.get_end();
+        if(end == -1) {
+            cout << "PETLA" << '\n';
+        } else {
+            //vector<int> result = g.find_shortest_odd_path_dfs(start, end);
+            vector<int> result = g.find_path_bfs(start, end);
+            if(result.empty()) {
+                cout << "NIE" << '\n';
+            } else {
+                cout << "TAK" << '\n';
+                for(int i = 0; i < result.size(); i++) {
+                    cout << result[i] << " ";
+                }
+                cout << '\n';
+            }
+        }
     }
     return 0;
 }
+
